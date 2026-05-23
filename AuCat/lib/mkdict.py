49356f216ac8f4d519459f2ab3361dcc071c814a
cow <cow.illuminati@cawez.ca>
2026-05-23 14:51:16 -0400

    Couleur DONE !

--- AuCat/lib/mkdict.py
+++ AuCat/lib/mkdict.py
@@ -2,24 +2,50 @@
 #Passe les fichiers un-par-un aux poids, et crée une Dict avec
 #TODO
 
-import poids.couleur.hue as hue
-import poids.couleur.hue_centre as hue_centre
-import poids.couleur.valeur as valeur
+import poids.couleur.combined as poids
 import numpy as np
+import os
 
 # Retourne le vecteur pour une vidéo
 def cat_vid(path, sample=10):
     vect = []
     
-    vect.append(valeur.valeur(path, sample))
+    hue, hue_c, val = poids.hue_centre_valeur(path, sample)
+
+    vect.append(val)
 
     # Les hues doivent êtres considérés sur un cercle! 
-    vect.append(np.exp((hue.hue(path, sample)/256)*2*np.pi*1j))
-    vect.append(np.exp((hue_centre.hue_centre(path,sample)/256)*2*np.pi*1j))
+    vect.append(np.exp(hue/256*2*np.pi*1j))
+    vect.append(np.exp(hue_c/256*2*np.pi*1j))
 
     return vect
 
 # Calcule chaque vidéo et assemble un dict
-def parse_vid(path):
-    pass
+def parse_vid(path, samples=10):
 
+    cedict = {}
+    
+    index = 0
+    nb = len(os.listdir(path))
+
+    print("Lecture de " + path + "...")
+    print(str(nb) + " fichiers trouvés...")
+
+    # Pour chaque vidéo
+    for clip in os.scandir(path):
+        # Progrès
+        print("\r" + str(int(index/nb * 100)) + "% : ", end="")
+
+        # C'est un clip (et non .. ou .)
+        if clip.is_file():
+            print(clip.name + (" " * 10), end="")
+            cedict[os.path.abspath(clip)] = cat_vid(os.path.abspath(clip),samples)
+
+        index += 1
+
+    print("\n\nTerminé..!")
+    
+    for key, value in cedict.items():
+        print(f"{key}: {value}")
+    
+    return cedict
---
+++ AuCat/lib/poids/couleur/combined.py
@@ -0,0 +1,88 @@
+
+import cv2
+import numpy as np
+
+
+## Décoder la vidéo prends 15+ secondes, donc on combine tout ici
+
+
+# Accepte un fichier et retourne le hue
+def hue_centre_valeur(path, samples=10):
+
+    # Charge la vidéo
+    video = cv2.VideoCapture(path, cv2.CAP_FFMPEG, [
+         cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY
+     ])
+
+    video.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
+
+    # On veut n images, trouvons la longeur 
+    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    n = int(length/samples)
+
+    # Préparation
+    images = []
+    index = 0
+
+    # Extraction
+    while True:
+        
+        if not video.grab():
+            break
+
+        if index % n == 0:
+            ret, frame = video.retrieve()
+
+            if not ret:
+                break
+    
+            images.append(frame)
+
+        index += 1
+
+    
+    hues_moy = []
+    hues_c_moy = []
+    vals_moy = []
+ 
+    # Crop
+
+    height, width, _ = images[0].shape
+    ymin = int(height / 3)
+    ymax = int(2 * height / 3)
+    xmin = int(width / 3)
+    xmax = int(2 * width / 3)
+
+   
+    for image in images:
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        
+        hsv_c = hsv[ymin:ymax, xmin:xmax]
+
+
+        hues = hsv[:, :, 0]
+        hues_moy.append(np.mean(hues))
+
+        hues_c = hsv_c[:, :, 0]
+        hues_c_moy.append(np.mean(hues_c))
+
+        vals = hsv[:, :, 2]
+        vals_moy.append(np.mean(vals))
+
+
+    hue_moy = 0
+    val_moy = 0
+    hue_c_moy = 0
+    for hue in hues_moy:
+        hue_moy += hue / len(hues_moy)
+
+    for hue_c in hues_c_moy:
+        hue_c_moy += hue_c / len(hues_c_moy)
+        
+    for val in vals_moy:
+        val_moy += val / len(vals_moy)
+
+    return hue_moy, hue_c_moy, val_moy
+        
+    
